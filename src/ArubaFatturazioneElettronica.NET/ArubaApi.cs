320cf706157a804901955b1b73c19fcea1254a35
lorenzo.basoc-kappas <lorenzo.basoc@gmail.com>
2023-11-15 16:16:54 +0100

    create endpoints

--- src/ArubaFatturazioneElettronica.NET/ArubaApi.cs
+++ src/ArubaFatturazioneElettronica.NET/ArubaApi.cs
@@ -1,3 +1,4 @@
+using ArubaFatturazioneElettronica.NET.Endpoints;
 using ArubaFatturazioneElettronica.NET.Interfaces;
 
 namespace ArubaFatturazioneElettronica.NET;
@@ -5,18 +6,22 @@ namespace ArubaFatturazioneElettronica.NET;
 public class ArubaApi : IArubaApi
 {
     public IAuth Auth { get; }
-
     public IFinancialCommunications FinancialCommunications { get; }
-
     public ISendInvoices SendInvoices { get; }
-
     public ISearchSentInvoices SearchSentInvoices { get; }
-
     public ISearchReceivedInvoices SearchReceivedInvoices { get; }
-
     public ISendOutcomeClient SendOutcomeClient { get; }
-
     public ISearchNotificationSentInvoices SearchNotificationSentInvoices { get; }
-
     public ISearchNotificationReceivedInvoices SearchNotificationReceivedInvoices { get; }
+
+    public ArubaApi() {
+        Auth = new Auth();
+        FinancialCommunications = new FinancialCommunications();
+        SendInvoices = new SendInvoices();
+        SearchSentInvoices = new SearchSentInvoices();
+        SearchReceivedInvoices = new SearchReceivedInvoices();
+        SendOutcomeClient = new SendOutcomeClient();
+        SearchNotificationSentInvoices = new SearchNotificationSentInvoices();
+        SearchNotificationReceivedInvoices = new SearchNotificationReceivedInvoices();
+    }
 }
---
+++ src/ArubaFatturazioneElettronica.NET/Endpoints/SearchReceivedInvoices.cs
@@ -0,0 +1,8 @@
+using ArubaFatturazioneElettronica.NET.Interfaces;
+
+namespace ArubaFatturazioneElettronica.NET.Endpoints;
+
+public class SearchReceivedInvoices : ISearchReceivedInvoices
+{
+
+}
