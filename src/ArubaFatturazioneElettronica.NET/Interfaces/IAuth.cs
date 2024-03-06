a56b6f06fa25019c2beb9c0a85d18ab91fd1371c
lorenzo.basoc-kappas <lorenzo.basoc@gmail.com>
2024-03-06 18:07:44 +0100

    update auth interface

--- src/ArubaFatturazioneElettronica.NET/Interfaces/IAuth.cs
+++ src/ArubaFatturazioneElettronica.NET/Interfaces/IAuth.cs
@@ -1,6 +1,10 @@
+using ArubaFatturazioneElettronica.NET.DTOs.Auth.Response;
+
 namespace ArubaFatturazioneElettronica.NET.Interfaces;
 
 public interface IAuth
 {
-
+    Task<UserInfoDto> GetUserInfo();
+    Task<MulticedentiDto> GetMulticedenti(string countryCode = null, string vatCode = null, string status = null, int size = 10, int page = 1);
+    Task<MulticedenteDto> GetMulticedenteById(string id);
 }
