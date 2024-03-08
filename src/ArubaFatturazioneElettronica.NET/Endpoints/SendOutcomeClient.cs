697f55be73253b8dff854a56aeab4337b0ceb65b
lorenzo.basoc-kappas <lorenzo.basoc@gmail.com>
2024-03-08 10:37:03 +0100

    send outcome client endpoint

--- src/ArubaFatturazioneElettronica.NET/Endpoints/SendOutcomeClient.cs
+++ src/ArubaFatturazioneElettronica.NET/Endpoints/SendOutcomeClient.cs
@@ -1,9 +1,24 @@
+using ArubaFatturazioneElettronica.NET.Constants;
+using ArubaFatturazioneElettronica.NET.DTOs.SendOutcomeClient.Request;
+using ArubaFatturazioneElettronica.NET.DTOs.SendOutcomeClient.Response;
 using ArubaFatturazioneElettronica.NET.Interfaces;
 using ArubaFatturazioneElettronica.NET.Services;
+using ArubaFatturazioneElettronica.NET.Utilities;
 
 namespace ArubaFatturazioneElettronica.NET.Endpoints;
 
-public class SendOutcomeClient(HttpService requester) : ISendOutcomeClient
+public class SendOutcomeClient(HttpService httpService) : ISendOutcomeClient
 {
-    private readonly HttpService _requester = requester;
+    private readonly HttpService _httpService = httpService;
+
+    public async Task<SendOutcomeClientResDto> SendEsitoCommittente(SendOutcomeClientReqDto dto) {
+        var data = HttpUtils.ComposePostBody(dto);
+        var responseDto = await _httpService.SendPostRequest<SendOutcomeClientResDto>(Urls.SendOutcomeClient.Send, data);
+        return responseDto;
+    }
+
+    public async Task<SendInvoiceOutcomeClientReqDto> StateOfSendEsitoCommittente(string filename) {
+        var responseDto = await _httpService.SendGetRequest<SendInvoiceOutcomeClientReqDto>(Urls.SendOutcomeClient.Send + $"/{filename}", null);
+        return responseDto;
+    }
 }
--- src/ArubaFatturazioneElettronica.NET/Interfaces/ISendOutcomeClient.cs
+++ src/ArubaFatturazioneElettronica.NET/Interfaces/ISendOutcomeClient.cs
@@ -1,6 +1,10 @@
+using ArubaFatturazioneElettronica.NET.DTOs.SendOutcomeClient.Request;
+using ArubaFatturazioneElettronica.NET.DTOs.SendOutcomeClient.Response;
+
 namespace ArubaFatturazioneElettronica.NET.Interfaces;
 
 public interface ISendOutcomeClient
 {
-
+    Task<SendOutcomeClientResDto> SendEsitoCommittente(SendOutcomeClientReqDto dto);
+    Task<SendInvoiceOutcomeClientReqDto> StateOfSendEsitoCommittente(string filename);
 }
